d99648e6cb9c19f3427b6d3adf34dc7a231eeba0
Junaidk007 <junaid.k0004@gmail.com>
2026-05-25 16:13:49 +0530

    added intractivity in searchbox

--- frontend/src/components/SearchBox.jsx
+++ frontend/src/components/SearchBox.jsx
@@ -1,21 +1,30 @@
+import { useRef, useState } from 'react';
 import './searchBox.css';
+import dayjs from "dayjs";
 
 function SearchBox() {
-    return ( 
+
+    const [checkInDate, setCheckIn] = useState("")
+    const [checkOutDate, setCheckOut] = useState("")
+
+    let checkIn = useRef(null)
+    let checkOut = useRef(null)
+
+    return (
         <div className="search-box">
             <div>
                 <label htmlFor="location">Location</label>
-                <input type="text" id="location" placeholder='Where are you going?' disabled/>
+                <input type="text" id="location" placeholder='Where are you going?' />
             </div>
-            <div>
+            <div onClick={() => checkIn.current?.showPicker()}>
                 <div>Check In</div>
-                <div className="addDate"></div>
-                <input type="date" id="check-in"  hidden />
+                <div className="addDate">{checkInDate == "" ? "Add date" : dayjs(checkInDate).format('DD - MMM')}</div>
+                <input ref={checkIn} onChange={() => setCheckIn(checkIn.current?.value)} type="date" id="check-in" />
             </div>
-            <div>
+            <div onClick={() => checkOut.current?.showPicker()}>
                 <div>Check Out</div>
-                <div className="addDate"></div>
-                <input type="date" id="check-out" hidden/>
+                <div className="addDate">{checkOutDate == "" ? "Add date" : dayjs(checkOutDate).format('DD - MMM')}</div>
+                <input ref={checkOut} onChange={() => setCheckOut(checkOut.current?.value)} type="date" id="check-out" />
             </div>
             <div className="search-btn">
                 <button type="button" disabled>
@@ -23,7 +32,7 @@ function SearchBox() {
                 </button>
             </div>
         </div>
-     );
+    );
 }
 
 export default SearchBox;
