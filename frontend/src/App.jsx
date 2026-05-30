e03b630338db91c2f83b04806bd7d18caccfc737
Junaidk007 <junaid.k0004@gmail.com>
2026-05-30 20:35:16 +0530

    made changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,10 +11,10 @@ function App() {
     <>
       <Navbar />
       <div className='appLayout'>
-        {/* <Home /> */}
+        <Home />
         <Listing/>
-        <Footer />
       </div>
+      <Footer />
     </>
   )
 }
