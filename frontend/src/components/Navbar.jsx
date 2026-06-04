58381a0e93409df6e33ef0856c4a8c9370d0ad49
Junaidk007 <junaid.k0004@gmail.com>
2026-06-04 16:12:52 +0530

    changed the theme of the website

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,24 +9,21 @@ function Navbar() {
         <nav className={`navbar ${isMenuOpen ? "active" : ""}`} >
             <div>
                 <div className="nav-container">
-                    <h2 className="nav-logo">BookMyBriks</h2>
+                    <h2 className="nav-logo"><Link to="/">BookMyBriks</Link></h2>
                     <div className="nav-menu-container">
                         <ul className="nav-menu">
                             <li className="nav-item">
-                                <a href="/" className="nav-link">Home</a>
+                                <Link to="/" className="nav-link">Home</Link>
                             </li>
                             <li className="nav-item">
-                                <a href="/about" className="nav-link">About</a>
+                                <Link to="/about" className="nav-link">About</Link>
                             </li>
                             <li className="nav-item">
-                                <a href="/contact" className="nav-link">Contact</a>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/add-listing" className="nav-link">Add Listing</Link>
+                                <Link to="/contact" className="nav-link">Contact</Link>
                             </li>
                         </ul>
                         <div className="login-cta">
-                            <button>Get Started</button>
+                            <Link to="/add-listing" className="nav-link"><button>Add Listing</button></Link>
                         </div>
                     </div>
                     <button className={`mobile-menu-toggle ${isMenuOpen ? "open" : ""}`} onClick={() => (setIsMenuOpen(!isMenuOpen))}>
